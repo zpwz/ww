f789a9d44792ea36e0ad79c94e97b707e1922473
zpwz <1226764058@qq.com>
2014-09-03 11:23:27 +0800

    冲突？

--- src/com/cn/Scs2.java
+++ src/com/cn/Scs2.java
@@ -6,7 +6,7 @@ public class Scs2 {
 		// TODO Auto-generated method stub
      System.out.println(123);
      System.out.println(456);
-    System.out.println(789);
+   System.out.println("³åÍ»~~~");
 	}
 
 }
